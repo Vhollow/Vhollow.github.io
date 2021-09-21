1e3146e317fa2a6675651835bd572231a501ee6c
Vicente Martínez Franco <Vhollow@users.noreply.github.com>
2021-09-21 17:28:33 +0200

    scripts deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:vhollow/Vhollow.github.io.git main
+git push -f git@github.com:vhollow/Vhollow.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
